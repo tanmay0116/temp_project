28b7dd3417bba70be1948d595b2b931ec23706eb
tanmay0116 <tanmaygoyal855@gmail.com>
2022-11-23 11:54:08 +0530

    v3

--- project.js
+++ project.js
@@ -1,6 +1,6 @@
 //TMDB 
 
-const API_KEY = 'api_key=343775a37f99a91f012bd0992bcc9130';
+const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
